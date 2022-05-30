7eb79c38ddbd4519009c5888cfd88d372b78b7c0
traacc <vaska.i.88@gmail.com>
2022-05-30 22:10:38 +0300

    refactor: added BEM names and changes some for classes and ids

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("addtask__input");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incomp_tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("comp_tasks");//completed-tasks
 
 
 //New task list item
